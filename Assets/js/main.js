a454bd811e18b3584cad7bac0a47aa27a54e99b9
Nguyenvanhieu203 <98386701+Nguyenvanhieu203@users.noreply.github.com>
2023-06-14 00:13:32 +0700

    testttt

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,21 +1,21 @@
-// Lấy tham chiếu đến danh sách các thẻ li
-var listItems = document.querySelectorAll('.navbar_items--list li');
+// // Lấy tham chiếu đến danh sách các thẻ li
+// var listItems = document.querySelectorAll('.navbar_items--list li');
 
-// Đặt lớp CSS "current-page" cho thẻ li của trang hiện tại
-function setCurrentPage() {
-  var currentPath = window.location.pathname;
+// // Đặt lớp CSS "current-page" cho thẻ li của trang hiện tại
+// function setCurrentPage() {
+//   var currentPath = window.location.pathname;
 
-  listItems.forEach(function(item) {
-    var link = item.querySelector('a');
-    var href = link.getAttribute('href');
+//   listItems.forEach(function(item) {
+//     var link = item.querySelector('a');
+//     var href = link.getAttribute('href');
 
-    if (href === currentPath) {
-      item.classList.add('current-page');
-    } else {
-      item.classList.remove('current-page');
-    }
-  });
-}
+//     if (href === currentPath) {
+//       item.classList.add('current-page');
+//     } else {
+//       item.classList.remove('current-page');
+//     }
+//   });
+// }
 // bắt sự kiện khi người dùng cuộn trang
 window.addEventListener('scroll', function() {
   var headerNavbar = document.querySelector('.header_navbar');
@@ -35,7 +35,7 @@ window.addEventListener('scroll', function() {
 
 // bắt sự kiện bật tắt input search
 // Gọi hàm setCurrentPage khi trang được tải
-window.addEventListener('DOMContentLoaded', setCurrentPage);
+window.addEventListener('DOMContentLoaded', HideShowSearch);
 
 // Bắt sự kiện bật btn search
 function HideShowSearch() {
